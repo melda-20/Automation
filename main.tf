06d980bc5cbe9c40f6c1d01df84a94fba46cf4a2
meli-hub <meldagumus-20@hotmail.com>
2024-09-19 13:26:32 +0200

    update

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "vsphere_virtual_machine" "web-server" {
   num_cpus = 2
   memory   = 2048
   guest_id = "ubuntu64Guest"
-  os_family = "Ubuntu Linux (64-bit)"
+  os_type = "other5xLinux64Guest"
 
   network_interface {
     network_id   = data.vsphere_network.network.id
