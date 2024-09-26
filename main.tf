2f650aa7e559cc108c1f408e16fb97315c2f1620
meli-hub <meldagumus-20@hotmail.com>
2024-09-26 13:51:03 +0200

    update

--- main.tf
+++ main.tf
@@ -1,12 +1,45 @@
+provider "vsphere" {
+  user                 = var.username_vsphere
+  password             = var.password_vsphere
+  vsphere_server       = var.url_vsphere
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = var.vsphere_resource_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_virtual_machine_template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 resource "vsphere_virtual_machine" "web-server" {
   name             = var.vsphere_virtual_machine_name
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
-  folder           = vsphere_folder.folder.path
+  folder           = "${var.vsphere_folder_path}"
 
-  num_cpus = 2
-  memory   = 2048
+  num_cpus = data.vsphere_virtual_machine.template.num_cpus
+  memory   = data.vsphere_virtual_machine.template.memory
   guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
     network_id   = data.vsphere_network.network.id
@@ -19,6 +52,8 @@ resource "vsphere_virtual_machine" "web-server" {
     thin_provisioned = true
   }
 
+
+
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
 
@@ -27,6 +62,13 @@ resource "vsphere_virtual_machine" "web-server" {
         host_name = "web-server"
         domain    = "local"
       }
+
+       network_interface {
+         ipv4_address = "10.0.10.100"
+         ipv4_netmask = 24
+       }
+
+       ipv4_gateway = "10.0.10.1"
 
       # Voeg de publieke SSH-sleutel toe aan de nieuwe server
       user_data = <<EOF
