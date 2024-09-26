98a2600ee60d050addef10319862c06dda5a8ea7
meli-hub <meldagumus-20@hotmail.com>
2024-09-26 12:44:28 +0200

    update

--- main.tf
+++ main.tf
@@ -1,51 +1,17 @@
-provider "vsphere" {
-  user                 = var.username_vsphere
-  password             = var.password_vsphere
-  vsphere_server       = var.url_vsphere
-  allow_unverified_ssl = true
+variable "ssh_public_key" {
+  description = "De publieke SSH-sleutel voor de nieuwe server"
+  type        = string
 }
 
-data "vsphere_datacenter" "dc" {
-  name = var.vsphere_datacenter
-}
-
-#resource "vsphere_folder" "folder" {
-#  path          = var.vsphere_folder_path
-#  type          = "vm"
-#  datacenter_id = data.vsphere_datacenter.dc.id
-#}
-
-data "vsphere_datastore" "datastore" {
-  name          = var.vsphere_datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_resource_pool" "pool" {
-  name          = var.vsphere_resource_pool
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_network" "network" {
-  name          = var.vsphere_network
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "template" {
-  name          = var.vsphere_virtual_machine_template
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-resource "vsphere_virtual_machine" "cloned_virtual_machine" {
+resource "vsphere_virtual_machine" "web-server" {
   name             = var.vsphere_virtual_machine_name
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
-  folder           = "${var.vsphere_folder_path}"
+  folder           = vsphere_folder.folder.path
 
-  num_cpus = data.vsphere_virtual_machine.template.num_cpus
-  memory   = data.vsphere_virtual_machine.template.memory
+  num_cpus = 2
+  memory   = 2048
   guest_id = data.vsphere_virtual_machine.template.guest_id
-
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
     network_id   = data.vsphere_network.network.id
@@ -61,19 +27,28 @@ resource "vsphere_virtual_machine" "cloned_virtual_machine" {
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
 
-    # Uncomment this if you need to customize the VM (network and hostname customization)
     customize {
-       linux_options {
-         host_name = "web-server"
-         domain    = "local"
-       }
+      linux_options {
+        host_name = "web-server"
+        domain    = "local"
+      }
 
-       network_interface {
-         ipv4_address = "10.0.10.100"
-         ipv4_netmask = 24
-       }
-
-       ipv4_gateway = "10.0.10.1"
-     }
+      # Voeg de publieke SSH-sleutel toe aan de nieuwe server
+      user_data = <<EOF
+      #cloud-config
+      users:
+        - name: ubuntu
+          ssh-authorized-keys:
+            - "${var.ssh_pub_key}"
+          sudo: ['ALL=(ALL) NOPASSWD:ALL']
+          groups: sudo
+          shell: /bin/bash
+      EOF
+    }
   }
+}
+
+output "vm_ip" {
+  description = "Het IP-adres van de nieuw aangemaakte VM"
+  value       = vsphere_virtual_machine.web-server.default_ip_address
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vm_ip" {
   description = "The IP-adres of the new VM"
-  value       = vsphere_virtual_machine.web-server.default_ip_address
+  value       = vsphere_virtual_machine.web-server.ip_address
 }
