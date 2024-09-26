63161b702f49c4993b67b1960912b908bf684b51
meli-hub <meldagumus-20@hotmail.com>
2024-09-26 15:40:07 +0200

    update

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vm_ip" {
-  description = "The IP-adres of the new VM"
-  value       = vsphere_virtual_machine.web-server.ip_address
+  description = "Het IP-adres van de aangemaakte VM"
+  value       = vsphere_virtual_machine.web-server.default_ip_address
 }
